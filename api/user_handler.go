9eb7618ede8116b890ffaa40d0ca4ab56a2a9a0c
Francis Badasu <nytro04@gmail.com>
2024-02-01 09:43:15 +0100

    feat: add create and read user functionality

---
+++ api/user_handler.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nytro04/hotel-reservation/db"
+	"github.com/nytro04/hotel-reservation/types"
+)
+
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+// constructor/factory function
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandlePostUser(c *fiber.Ctx) error {
+	var params types.CreateUserParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+	user, err := types.NewUserFromParams(params)
+	if err != nil {
+		return err
+	}
+	insertedUser, err := h.userStore.InsertUser(c.Context(), user)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(insertedUser)
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id = c.Params("id")
+	)
+
+	user, err := h.userStore.GetUserByID(c.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
+	users, err := h.userStore.GetUsers(c.Context())
+	if err != nil {
+		return err
+	}
+	return c.JSON(users)
+}
--- main.go
+++ main.go
@@ -1,29 +1,45 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/nytro04/hotel-reservation/api"
+	"github.com/nytro04/hotel-reservation/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
+
 	listenAddr := flag.String("listenAddr", ":9999", "The listen address for the API Server")
 
-	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 
 	apiV1 := app.Group("/api/v1")
 
-	app.Get("/foo", handleFoo)
-
-	apiV1.Get("/user", handleUser)
+	apiV1.Post("/user", userHandler.HandlePostUser)
+	apiV1.Get("/user", userHandler.HandleGetUsers)
+	apiV1.Get("/user/:id", userHandler.HandleGetUser)
 
 	app.Listen(*listenAddr)
-}
-
-func handleFoo(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"msg": "working just fine"})
-}
-
-func handleUser(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"msg": "user foo"})
 }
